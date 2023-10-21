0edd6cf2be8a54194da13c21af559a6aa0ba58f9
DieselAponte <143888820+DieselAponte@users.noreply.github.com>
2023-10-21 12:04:20 -0500

    Diesel Aponte

--- Proyecto-Banco/src/proyecto/banco/CuentaBancaria.java
+++ Proyecto-Banco/src/proyecto/banco/CuentaBancaria.java
@@ -19,5 +19,66 @@ public class CuentaBancaria {
         this.estado = "Activo"; // Por defecto, la cuenta se crea como "Activo"
     }
     
+    // Métodos
+    public void calcularInteresMensual() {
+        // Implementa el cálculo de intereses mensuales aquí
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public String getDescripcionCuenta() {
+        return descripcionCuenta;
+    }
+
+    public void setDescripcionCuenta(String descripcionCuenta) {
+        this.descripcionCuenta = descripcionCuenta;
+    }
+
+    public String getCuentaCCI() {
+        return cuentaCCI;
+    }
+
+    public void setCuentaCCI(String cuentaCCI) {
+        this.cuentaCCI = cuentaCCI;
+    }
+
+    public double getSaldoDisponible() {
+        return saldoDisponible;
+    }
+
+    public void setSaldoDisponible(double saldo) {
+        this.saldoDisponible = saldo;
+    }
+
+    public ArrayList<MovimientoCuenta> getMovimientos() {
+        return movimientos;
+    }
+
+    public void agregarMovimiento(MovimientoCuenta movimiento) {
+        movimientos.add(movimiento);
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "CuentaBancaria [Número de Cuenta: " + numeroCuenta +
+               ", Descripción: " + descripcionCuenta +
+               ", Cuenta CCI: " + cuentaCCI +
+               ", Saldo Disponible: " + saldoDisponible +
+               ", Estado: " + estado + "]";
+    }
 }
 
