fc3035cd82c798cfadf5052de95fc07db89c3374
JesusCmg <142682642+JesusCmgCode@users.noreply.github.com>
2023-10-21 12:37:12 -0500

    Jesus Camargo

--- Proyecto-Banco/src/proyecto/banco/EventoBancario.java
+++ Proyecto-Banco/src/proyecto/banco/EventoBancario.java
@@ -8,4 +8,42 @@ class EventoBancario {
     private String tituloEvento;
     private String descripcionEvento;
 
+    // Constructor
+    public EventoBancario(Date fecha, String tituloEvento, String descripcionEvento) {
+        this.fecha = fecha;
+        this.tituloEvento = tituloEvento;
+        this.descripcionEvento = descripcionEvento;
+    }
+
+    // Getters y setters
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTituloEvento() {
+        return tituloEvento;
+    }
+
+    public void setTituloEvento(String tituloEvento) {
+        this.tituloEvento = tituloEvento;
+    }
+
+    public String getDescripcionEvento() {
+        return descripcionEvento;
+    }
+
+    public void setDescripcionEvento(String descripcionEvento) {
+        this.descripcionEvento = descripcionEvento;
+    }
+
+    @Override
+    public String toString() {
+        return "EventoBancario [Fecha: " + fecha +
+               ", Título del Evento: " + tituloEvento +
+               ", Descripción del Evento: " + descripcionEvento + "]";
+    }
 }
