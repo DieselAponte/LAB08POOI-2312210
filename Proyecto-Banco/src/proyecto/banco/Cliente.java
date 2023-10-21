0157e83ee57e2ce593bc14c5e5cd151975fdfc40
JesusCmg <142682642+JesusCmgCode@users.noreply.github.com>
2023-10-21 12:34:32 -0500

    Jesús Camargo

---
+++ Proyecto-Banco/src/proyecto/banco/Cliente.java
@@ -0,0 +1,18 @@
+
+package proyecto.banco;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Cliente {
+    private String idCliente;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private int telefono;
+    private int dni;
+    private ArrayList<CuentaBancaria> cuentas;
+    private ArrayList<EventoBancario> eventosBancarios;
+
+    
+}
+
