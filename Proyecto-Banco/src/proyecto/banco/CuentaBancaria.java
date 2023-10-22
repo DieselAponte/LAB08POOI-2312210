8a12da5ebe77c76d50fcb4f411aa423a1ae74ac6
DieselAponte <143888820+DieselAponte@users.noreply.github.com>
2023-10-22 18:55:23 -0500

    Add files via upload

--- Proyecto-Banco/src/proyecto/banco/CuentaBancaria.java
+++ Proyecto-Banco/src/proyecto/banco/CuentaBancaria.java
@@ -11,5 +11,59 @@ public class CuentaBancaria {
     private List<MovimientoCuenta> movimientos = new ArrayList<>();
     private String estado;
 
-    
+    public CuentaBancaria(String numeroCuenta, String descripcionCuenta, String cuentaCCI, Cliente cliente) {
+    this.numeroCuenta = numeroCuenta;
+    this.descripcionCuenta = descripcionCuenta;
+    this.cuentaCCI = cuentaCCI;
+    cliente.addCuenta(this);
+}
+
+
+    public void calcularInteresMensual() {
+        double tasaInteresAnual = 0.03; // Cambiar según la tasa de interés
+        double interesMensual = (saldoDisponible * tasaInteresAnual) / 12;
+        saldoDisponible += interesMensual;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public String getDescripcionCuenta() {
+        return descripcionCuenta;
+    }
+
+    public void setDescripcionCuenta(String descripcionCuenta) {
+        this.descripcionCuenta = descripcionCuenta;
+    }
+
+    public String getCuentaCCI() {
+        return cuentaCCI;
+    }
+
+    public void setCuentaCCI(String cuentaCCI) {
+        this.cuentaCCI = cuentaCCI;
+    }
+
+    public double getSaldoDisponible() {
+        return saldoDisponible;
+    }
+
+    public void setSaldoDisponible(double saldoDisponible) {
+        this.saldoDisponible = saldoDisponible;
+    }
+
+    public List<MovimientoCuenta> getMovimientos() {
+        return movimientos;
+    }
+
+    @Override
+    public String toString() {
+        return "CuentaBancaria [numeroCuenta=" + numeroCuenta + ", descripcionCuenta=" + descripcionCuenta
+                + ", cuentaCCI=" + cuentaCCI + ", saldoDisponible=" + saldoDisponible + ", estado=" + estado + "]";
+    }
 }
