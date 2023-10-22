c753b7872fc852f11f300b1f2eead1e17ffcf9b0
DieselAponte <143888820+DieselAponte@users.noreply.github.com>
2023-10-22 18:54:03 -0500

    Diesel Aponte

---
+++ Proyecto-Banco/CuentaBancaria.java
@@ -0,0 +1,69 @@
+package proyecto.banco;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class CuentaBancaria {
+    private String numeroCuenta;
+    private String descripcionCuenta;
+    private String cuentaCCI;
+    private double saldoDisponible = 0;
+    private List<MovimientoCuenta> movimientos = new ArrayList<>();
+    private String estado;
+
+    public CuentaBancaria(String numeroCuenta, String descripcionCuenta, String cuentaCCI, Cliente cliente) {
+    this.numeroCuenta = numeroCuenta;
+    this.descripcionCuenta = descripcionCuenta;
+    this.cuentaCCI = cuentaCCI;
+    cliente.addCuenta(this);
+}
+
+
+    public void calcularInteresMensual() {
+        double tasaInteresAnual = 0.03; // Cambiar según la tasa de interés
+        double interesMensual = (saldoDisponible * tasaInteresAnual) / 12;
+        saldoDisponible += interesMensual;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public String getDescripcionCuenta() {
+        return descripcionCuenta;
+    }
+
+    public void setDescripcionCuenta(String descripcionCuenta) {
+        this.descripcionCuenta = descripcionCuenta;
+    }
+
+    public String getCuentaCCI() {
+        return cuentaCCI;
+    }
+
+    public void setCuentaCCI(String cuentaCCI) {
+        this.cuentaCCI = cuentaCCI;
+    }
+
+    public double getSaldoDisponible() {
+        return saldoDisponible;
+    }
+
+    public void setSaldoDisponible(double saldoDisponible) {
+        this.saldoDisponible = saldoDisponible;
+    }
+
+    public List<MovimientoCuenta> getMovimientos() {
+        return movimientos;
+    }
+
+    @Override
+    public String toString() {
+        return "CuentaBancaria [numeroCuenta=" + numeroCuenta + ", descripcionCuenta=" + descripcionCuenta
+                + ", cuentaCCI=" + cuentaCCI + ", saldoDisponible=" + saldoDisponible + ", estado=" + estado + "]";
+    }
+}
