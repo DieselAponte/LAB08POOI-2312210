1ff7c31da2d80a17b6e5038ec4a41509d90b6aed
DanielCastroG <142682831+DanielCastroG@users.noreply.github.com>
2023-10-22 19:22:09 -0500

    Update CuentaSueldo.java
    Daniel Castro

--- Proyecto-Banco/src/proyecto/banco/CuentaSueldo.java
+++ Proyecto-Banco/src/proyecto/banco/CuentaSueldo.java
@@ -1,13 +1,35 @@
 package proyecto.banco;
 
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CuentaSueldo extends CuentaAhorro {
-    private String nombreEmpresa;
 
-    public CuentaSueldo(String numeroCuenta, String descripcionCuenta, String cuentaCCI, Cliente cliente,
-                        double tasaInteresAnual, String[] beneficios, String nombreEmpresa) {
-        super(numeroCuenta, descripcionCuenta, cuentaCCI, cliente, tasaInteresAnual, beneficios);
+    private String nombreEmpresa;
+    private List<MovimientoCuenta> movimientos;
+    private int retirosEnCajero;
+    private List<String> beneficios;
+    private Cliente cliente; 
+    private double maxRetiro;
+    
+    public CuentaSueldo(String nombreEmpresa, Cliente cliente, double saldoInicial, double maxRetiro) {
+        super(0.03, new String[]{});
         this.nombreEmpresa = nombreEmpresa;
+        this.cliente = cliente; 
+        this.maxRetiro = maxRetiro;
+        this.movimientos = new ArrayList<>();
+        this.retirosEnCajero = 0;
+        this.beneficios = new ArrayList<>();
+        this.setSaldoDisponible(saldoInicial);
+    }
+
+    public double getMaxRetiro() {
+        return maxRetiro;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
     }
 
     public String getNombreEmpresa() {
@@ -18,15 +40,38 @@ public class CuentaSueldo extends CuentaAhorro {
         this.nombreEmpresa = nombreEmpresa;
     }
 
-    @Override
-    public String toString() {
-        return "Cuenta Sueldo [Número de Cuenta: " + getNumeroCuenta() +
-               ", Descripción: " + getDescripcionCuenta() +
-               ", Cuenta CCI: " + getCuentaCCI() +
-               ", Saldo Disponible: " + getSaldoDisponible() +
-               ", Tasa de Interés Anual: " + getTasaInteresAnual() +
-               ", Beneficios: " + java.util.Arrays.toString(getBeneficios()) +
-               ", Nombre de la Empresa: " + nombreEmpresa +
-               ", Estado: " + getEstado() + "]";
+    public int getRetirosEnCajero() {
+        return retirosEnCajero;
+    }
+
+    public String[] getBeneficios() {
+        return beneficios.toArray(new String[0]);
+    }
+
+    public void registrarRetiroCajero(double monto) {
+        if (retirosEnCajero < 5) {
+            if (monto <= getSaldoDisponible()) {
+                setSaldoDisponible(getSaldoDisponible() - monto);
+                movimientos.add(new MovimientoCuenta("Operación en cajero", "Retiro en cajero", new Date(), monto, "Débito"));
+                retirosEnCajero++;
+            } else {
+                System.out.println("Saldo insuficiente para el retiro en cajero.");
+            }
+        } else {
+            System.out.println("Se ha alcanzado el límite de retiros en cajero.");
+        }
+    }
+
+    public void agregarBeneficio(String beneficio) {
+        if (beneficios.size() < 5) {
+            beneficios.add(beneficio);
+            System.out.println("Beneficio agregado: " + beneficio);
+        } else {
+            System.out.println("Se ha alcanzado el límite de beneficios para la cuenta.");
+        }
+    }
+
+    public List<MovimientoCuenta> getMovimientos() {
+        return movimientos;
     }
 }
